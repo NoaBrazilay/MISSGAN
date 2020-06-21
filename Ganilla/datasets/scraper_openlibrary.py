7e039e217ca3829c83f4465d9961ab3a85c3651b
Noa barzilay <noabarzilay11@gmail.com>
2020-06-21 16:11:50 +0300

    Fix all scraper bugs

--- Ganilla/datasets/scraper_openlibrary.py
+++ Ganilla/datasets/scraper_openlibrary.py
@@ -46,156 +46,198 @@ class OpenLibHelper(object):
         searchElem.send_keys(author)
         searchElem.send_keys(Keys.RETURN)
         # wait up to 10 seconds for the elements to become available
-        randdelay(5, 7)
+        randdelay(9, 12)
 
-        ## search results
-        search_res = self.browser.find_element_by_id("searchResults")
-        elementList = search_res.find_elements_by_tag_name("li")
+        for i in range(0,2):
+            if i==1:
+                two_button = self.browser.find_element_by_link_text('2')
+                two_button.click()
+                randdelay(9, 11)
 
-        # traverse results
-        for i in range(len(elementList)):
-            element = self.browser.find_element_by_id("searchResults").find_elements_by_tag_name("li")[i]
-            try:
+            ## search results
+            search_res = self.browser.find_element_by_id("searchResults")
+            elementList = search_res.find_elements_by_tag_name("li")
 
-                # Save the window opener (current window)
-                main_window = self.browser.current_window_handle
-
-                # open result in new tab
-                # borrow_button = element.find_element_by_xpath("//div[@class='searchResultItemCTA']//div[@class='searchResultItemCTA-lending']")
-                borrow_button_first = element.find_element_by_tag_name('a')
-                borrow_button_first.send_keys(Keys.CONTROL + Keys.RETURN)
-                randdelay(5, 7) # wait for new tab to load
-
-                # Get windows list and put focus on new window (which is on the 1st index in the list)
-                windows = self.browser.window_handles
-                self.browser.switch_to.window(windows[1])
-                # do whatever you have to do on this page, we will just got to sleep for now
-                randdelay(7, 11)
-
-                title = self.browser.find_element_by_xpath(
-                    "//body[@id='user']//div[@id='test-body-mobile']//div[@class='contentContainer']//div[@id='contentHead']//span[@itemprop='name']").text
-                if (book_list) and (not title.lower() in book_list):
-                    raise Exception('Book is not in the list!')
-
-                dir_name = data_folder + "/" + title
-                # if we already downloaded this book, move on!!
-                if os.path.exists(dir_name):
-                    raise Exception('Already downloaded this book!')  # Don't! If you catch, likely to hide bugs.
-                else: # else lets create a dir and download!
-                    os.makedirs(dir_name)
-
-                #now click Borrow!! or maybe we already loaned this so try to read at least.
+            # traverse results
+            for i in range(len(elementList)):
+                element = self.browser.find_element_by_id("searchResults").find_elements_by_tag_name("li")[i]
                 try:
-                    borrow_button_second = self.browser.find_element_by_link_text('Borrow')
-                except:
-                    # try:
-                    borrow_button_second = self.browser.find_element_by_link_text('Read')
-                    # except:
-                    #     continue # no loans available so move to the next book.
 
-                borrow_button_second.click()
-                randdelay(20, 23)
-                #switch to one page
-                one_page = self.browser.find_element_by_xpath(
-                    "//div[@id='IABookReaderWrapper']//div[@id='BookReader']//div[@class='BRfooter']//div[@class='BRnav BRnavDesktop']//div[@class='BRpage']//button[@bt-xtitle='One-page view']")
-                one_page.click()
+                    # Save the window opener (current window)
+                    main_window = self.browser.current_window_handle
 
-                # now take source text, we will use it to generate full URLs
-                page_data = self.browser.page_source
-                page_list = page_data.splitlines()
-                # find the item index similar to this:
-                # url: '//ia903101.us.archive.org/BookReader/BookReaderJSIA.php?id=lettherebelight0000unse&itemPath=
-                # /5/items/lettherebelight0000unse&server=ia903101.us.archive.org&format=jsonp&subPrefix=
-                # lettherebelight0000unse&requestUri=/stream/lettherebelight0000unse&version=mHe9koCz',
-                str___ = page_list[267]  # this index may change if site is updated!
+                    # open result in new tab
+                    # borrow_button = element.find_element_by_xpath("//div[@class='searchResultItemCTA']//div[@class='searchResultItemCTA-lending']")
+                    borrow_button_first = element.find_element_by_tag_name('a')
+                    borrow_button_first.send_keys(Keys.CONTROL + Keys.RETURN)
+                    randdelay(5, 7) # wait for new tab to load
 
-                aaa = str___.replace(" ", "") # remove heading whitespaces
-                tokens_url = re.split(';|,|\.|/|=|&|', aaa) # now split into tokens
-                # tk0 = tokens_url[0]
-                # tk1 = tokens_url[1]
-                tk2 = tokens_url[2] # this is similar to ia903101
-                tk3 = tokens_url[3] # us
-                tk4 = tokens_url[4] # archive
-                tk5 = tokens_url[5] # org
-                tk6 = tokens_url[6] # BookReader
-                tk12 = tokens_url[12]
-                tk13 = tokens_url[13]
-                tk14 = tokens_url[14]
 
-                # here turn pages till end!!!
-                for j in range(1000): #adjust this to pages of a book
-                    # print self.browser.current_url
-                    # str2 = "https://ia800706.us.archive.org/BookReader/BookReaderImages.php?zip=/35/items/billpeetautobiog00peet/billpeetautobiog00peet_jp2.zip&file=billpeetautobiog00peet_jp2/billpeetautobiog00peet_0019.jp2&scale=1&rotate=0"
-                    page_num = '0001'
-                    if j < 9:
-                        page_num = '000' + str(j+1)
-                    elif j < 99:
-                        page_num = '00' + str(j+1)
-                    else:
-                        page_num = '0' + str(j+1)
-
-                    file_path = dir_name + "/page_{PAGE_NUM}.png".format(PAGE_NUM=page_num)
-
-                    source_URL = "https://" + tk2 + '.' + tk3 + '.' + tk4 + '.' + \
-                                 tk5 + '/' + tk6 + "/BookReaderImages.php?zip=/" + tk12 + "/" + \
-                                 tk13 + "/" + tk14 + '/' + tk14 + "_jp2.zip&file=" + \
-                                 tk14 + "_jp2/" + tk14 + "_{PAGE_NUM}".format(PAGE_NUM=page_num) + ".jp2&scale=1&rotate=0"
-
-                    scripttt = '''window.open('{link}')'''.format(link=source_URL) #,'_blank'
-                    self.browser.execute_script(scripttt)
-                    # Get windows list and put focus on new window (which is on the 2nd index in the list)
+                    # Get windows list and put focus on new window (which is on the 1st index in the list)
                     windows = self.browser.window_handles
-                    self.browser.switch_to.window(windows[2])
-                    self.browser.maximize_window()
+                    self.browser.switch_to.window(windows[1])
+                    # do whatever you have to do on this page, we will just got to sleep for now
+                    randdelay(5, 10)
 
-                    randdelay(15, 18) # wait for page to load
+
+                    title = self.browser.find_element_by_xpath(
+                        "//body[@id='user']//div[@id='test-body-mobile']//div[@class='contentContainer']//div[@id='contentHead']//span[@itemprop='name']").text
+                    if (book_list) and (not title.lower().replace("í", "") in book_list):
+                        raise Exception('Book is not in the list!')
+
+                    dir_name = data_folder + "/" + title
+                    dir_name = dir_name.replace("?", "")
+                    dir_name = dir_name.replace("'", "")
+                    dir_name = dir_name.replace(":", "")
+                    dir_name = dir_name.replace("í", "")
+
+                    # if we already downloaded this book, move on!!
+                    if os.path.exists(dir_name):
+                        raise Exception('Already downloaded this book!')  # Don't! If you catch, likely to hide bugs.
+                    else: # else lets create a dir and download!
+                        os.makedirs(dir_name)
+
+                    #now click Borrow!! or maybe we already loaned this so try to read at least.
+                    try:
+                        check_button_second = self.browser.find_element_by_link_text('Check Availability')
+                        check_button_second.click()
+                        randdelay(10, 13)
+                        borrow_button_second = self.browser.find_element_by_link_text('1 Hour Borrow')
+                    except:
+                        try:
+                            borrow_button_second = self.browser.find_element_by_link_text('Borrow')
+                        except:
+                            # try:
+                            borrow_button_second = self.browser.find_element_by_link_text('Read')
+                            # except:
+                            #     continue # no loans available so move to the next book.
+
+                    borrow_button_second.click()
+                    randdelay(20, 23)
 
                     try:
-                        img = self.browser.find_element_by_tag_name('img')
-                        # now lets get full image!!!
-                        self.browser.execute_script("arguments[0].setAttribute('class','overflowingVertical')", img)
-                        self.browser.execute_script("arguments[0].removeAttribute('width','')", img)
-                        self.browser.execute_script("arguments[0].removeAttribute('height','')", img)
-
-                        img.screenshot(file_path)
-                        randdelay(3, 5)
+                        borrow_1h_button_second = self.browser.find_element_by_link_text('Borrow for 1 hour')
+                        borrow_1h_button_second.click()
+                        randdelay(15, 20)
                     except:
+                        pass
+                    try:
+                        borrow_lg_button_second = self.browser.find_element_by_link_text('Log In and Borrow')
+                        borrow_lg_button_second.click()
+                        randdelay(7, 10)
+                        emailElem = self.browser.find_element_by_name('username')
+                        emailElem.send_keys('noabarzilay11@gmail.com')
+                        passwordElem = self.browser.find_element_by_name('password')
+                        passwordElem.send_keys('Nn654321')
+                        passwordElem.send_keys(Keys.RETURN)
+                        randdelay(5, 10)
+                    except:
+                        pass
+                    #switch to one page
+                    one_page = self.browser.find_element_by_xpath(
+                        "//div[@id='IABookReaderWrapper']//div[@id='BookReader']//div[@class='BRfooter']//div[@class='BRnav BRnavDesktop']//div[@class='BRpage']//button[@bt-xtitle='One-page view']")
+                    one_page.click()
+
+                    # now take source text, we will use it to generate full URLs
+                    page_data = self.browser.page_source
+                    page_list = page_data.splitlines()
+                    # find the item index similar to this:
+                    # url: '//ia903101.us.archive.org/BookReader/BookReaderJSIA.php?id=lettherebelight0000unse&itemPath=
+                    # /5/items/lettherebelight0000unse&server=ia903101.us.archive.org&format=jsonp&subPrefix=
+                    # lettherebelight0000unse&requestUri=/stream/lettherebelight0000unse&version=mHe9koCz',
+                    str___ = page_list[452]  # this index may change if site is updated!
+
+
+                    if not (("url" in str___) and ("ia" in str___)):
+                        str___ = page_list[446]  # this index may change if site is updated!
+
+                    aaa = str___.replace(" ", "") # remove heading whitespaces
+                    tokens_url = re.split(';|,|\.|/|=|&', aaa) # now split into tokens
+                    # tk0 = tokens_url[0]
+                    # tk1 = tokens_url[1]
+                    tk2 = tokens_url[2] # this is similar to ia903101
+                    tk3 = tokens_url[3] # us
+                    tk4 = tokens_url[4] # archive
+                    tk5 = tokens_url[5] # org
+                    tk6 = tokens_url[6] # BookReader
+                    tk12 = tokens_url[12]
+                    tk13 = tokens_url[13]
+                    tk14 = tokens_url[14]
+
+                    # here turn pages till end!!!
+                    for j in range(1000): #adjust this to pages of a book
+                        # print self.browser.current_url
+                        # str2 = "https://ia800706.us.archive.org/BookReader/BookReaderImages.php?zip=/35/items/billpeetautobiog00peet/billpeetautobiog00peet_jp2.zip&file=billpeetautobiog00peet_jp2/billpeetautobiog00peet_0019.jp2&scale=1&rotate=0"
+                        page_num = '0001'
+                        if j < 9:
+                            page_num = '000' + str(j+1)
+                        elif j < 99:
+                            page_num = '00' + str(j+1)
+                        else:
+                            page_num = '0' + str(j+1)
+
+                        file_path = dir_name + "/page_{PAGE_NUM}.png".format(PAGE_NUM=page_num)
+
+                        source_URL = "https://" + tk2 + '.' + tk3 + '.' + tk4 + '.' + \
+                                     tk5 + '/' + tk6 + "/BookReaderImages.php?zip=/" + tk12 + "/" + \
+                                     tk13 + "/" + tk14 + '/' + tk14 + "_jp2.zip&file=" + \
+                                     tk14 + "_jp2/" + tk14 + "_{PAGE_NUM}".format(PAGE_NUM=page_num) + ".jp2&scale=1&rotate=0"
+
+                        scripttt = '''window.open('{link}')'''.format(link=source_URL) #,'_blank'
+                        self.browser.execute_script(scripttt)
+                        # Get windows list and put focus on new window (which is on the 2nd index in the list)
+                        windows = self.browser.window_handles
+                        self.browser.switch_to.window(windows[2])
+                        self.browser.maximize_window()
+
+                        randdelay(15, 18) # wait for page to load
+
+                        try:
+                            img = self.browser.find_element_by_tag_name('img')
+                            # now lets get full image!!!
+                            self.browser.execute_script("arguments[0].setAttribute('class','overflowingVertical')", img)
+                            self.browser.execute_script("arguments[0].removeAttribute('width','')", img)
+                            self.browser.execute_script("arguments[0].removeAttribute('height','')", img)
+
+                            img.screenshot(file_path)
+                            randdelay(3, 5)
+                        except:
+                            # Close current window
+                            self.browser.close()
+                            # get back to prev window!!!
+                            windows = self.browser.window_handles
+                            self.browser.switch_to.window(windows[1])
+                            break # break from the loop
+
                         # Close current window
                         self.browser.close()
+
                         # get back to prev window!!!
                         windows = self.browser.window_handles
                         self.browser.switch_to.window(windows[1])
-                        break # break from the loop
 
+                    # now we can return our book!!
+                    self.browser.find_element_by_link_text("Return Book").click()
+                    randdelay(1, 2)
+                    # close float tab, and go back to main window
+                    try:
+                        return_button = self.browser.find_element_by_xpath("//div[@id='colorbox']//div[@id='cboxWrapper']//div[@id='cboxContent']//div[@id='cboxLoadedContent']//div[@class='center BRfloat']//div[@class='center BRfloatFoot']//button[@class='action red']")
+                        return_button.click()
+                        randdelay(8, 11)
+                        # Close current window
+                        self.browser.close()
+                        # Put focus back on main window
+                        self.browser.switch_to.window(main_window)
+                    except:
+                        print ("no return button")
+
+                    randdelay(3, 5)
+                except:
                     # Close current window
-                    self.browser.close()
-
-                    # get back to prev window!!!
-                    windows = self.browser.window_handles
-                    self.browser.switch_to.window(windows[1])
-
-                # now we can return our book!!
-                self.browser.find_element_by_link_text("Return Book").click()
-                randdelay(1, 2)
-                # close float tab, and go back to main window
-                try:
-                    return_button = self.browser.find_element_by_xpath("//div[@id='colorbox']//div[@id='cboxWrapper']//div[@id='cboxContent']//div[@id='cboxLoadedContent']//div[@class='center BRfloat']//div[@class='center BRfloatFoot']//button[@class='action red']")
-                    return_button.click()
-                    randdelay(8, 11)
-                    # Close current window
-                    self.browser.close()
+                    self.close()
                     # Put focus back on main window
                     self.browser.switch_to.window(main_window)
-                except:
-                    print ("no return button")
-
-                randdelay(3, 5)
-            except:
-                # Close current window
-                self.close()
-                # Put focus back on main window
-                self.browser.switch_to.window(main_window)
-                continue
+                    continue
 
     def close(self):
         self.browser.close()
