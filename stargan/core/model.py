f5ee12e86a14f366af8f3f7f998a5f044afba258
Noa barzilay <noabarzilay11@gmail.com>
2020-08-23 16:15:58 +0300

    Add Ganilla Generator

--- stargan/core/model.py
+++ stargan/core/model.py
@@ -119,6 +119,45 @@ class AdainResBlk(nn.Module):
             out = (out + self._shortcut(x)) / math.sqrt(2)
         return out
 
+class AdainResBlkGanilla(nn.Module):
+    def __init__(self, dim_in, dim_out, style_dim=64, actv=nn.LeakyReLU(0.2), upsample=True):
+        super().__init__()
+
+        self.actv = actv
+        self.upsample = upsample
+        self.learned_sc = dim_in != dim_out
+        self._build_weights(dim_in, dim_out, style_dim)
+
+    def _build_weights(self, dim_in, dim_out, style_dim=64):
+        self.conv1 = nn.Conv2d(dim_in, dim_out, 3, 1, 1)
+        self.conv2 = nn.Conv2d(dim_out, dim_out, 3, 1, 1)
+        self.norm1 = AdaIN(style_dim, dim_out)
+        self.norm2 = AdaIN(style_dim, dim_out)
+        if self.learned_sc:
+            self.conv1x1 = nn.Conv2d(dim_in, dim_out, 1, 1, 0, bias=False)
+
+    def _shortcut(self, x):
+        if self.upsample:
+            x = F.interpolate(x, scale_factor=2, mode='nearest')
+        if self.learned_sc:
+            x = self.conv1x1(x)
+        return x
+
+    def _residual(self, x, s):
+        x = self.conv1(x)
+        x = self.norm1(x, s)
+        x = self.actv(x)
+        if self.upsample:
+            x = F.interpolate(x, scale_factor=2, mode='nearest')
+        x = self.conv2(x)
+        x = self.norm2(x, s)
+        x = self.actv(x)
+        return x
+
+    def forward(self, x, s):
+        out = self._residual(x, s)
+        #out = (out + self._shortcut(x)) / math.sqrt(2)
+        return out
 
 class HighPass(nn.Module):
     def __init__(self, w_hpf, device):
@@ -185,6 +224,168 @@ class Generator(nn.Module):
                 x = x + self.hpf(mask * cache[x.size(2)])
         return self.to_rgb(x)
 
+
+class BasicBlock_Ganilla(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, use_dropout, stride=1):
+        super(BasicBlock_Ganilla, self).__init__()
+        self.rp1 = nn.ReflectionPad2d(1)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=0, bias=False)
+        self.bn1 = nn.InstanceNorm2d(planes)
+        self.use_dropout = use_dropout
+        if use_dropout:
+            self.dropout = nn.Dropout(0.5)
+        self.rp2 = nn.ReflectionPad2d(1)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=0, bias=False)
+        self.bn2 = nn.InstanceNorm2d(planes)
+        self.out_planes = planes
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.InstanceNorm2d(self.expansion*planes)
+            )
+
+            self.final_conv = nn.Sequential(
+                nn.ReflectionPad2d(1),
+                nn.Conv2d(self.expansion * planes * 2, self.expansion * planes, kernel_size=3, stride=1,
+                                        padding=0, bias=False),
+                nn.InstanceNorm2d(self.expansion * planes)
+            )
+        else:
+            self.final_conv = nn.Sequential(
+                nn.ReflectionPad2d(1),
+                nn.Conv2d(planes*2, planes, kernel_size=3, stride=1, padding=0, bias=False),
+                nn.InstanceNorm2d(planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(self.rp1(x))))
+        if self.use_dropout:
+            out = self.dropout(out)
+        out = self.bn2(self.conv2(self.rp2(out)))
+        inputt = self.shortcut(x)
+        catted = torch.cat((out, inputt), 1)
+        out = self.final_conv(catted)
+        out = F.relu(out)
+        return out
+
+class PyramidFeatures(nn.Module):
+    def __init__(self, C2_size, C3_size, C4_size, C5_size, fpn_weights =[1.0, 1.0, 1.0, 1.0], feature_size=128, style_dim=64):
+        super().__init__()
+
+        self.sum_weights = fpn_weights #[1.0, 0.5, 0.5, 0.5]
+
+        # upsample C5 to get P5 from the FPN paper
+        self.P5_1 = nn.Conv2d(C5_size, feature_size, kernel_size=1, stride=1, padding=0)
+        self.P5_2 = AdainResBlkGanilla(C5_size, feature_size, style_dim)
+
+        # add P5 elementwise to C4
+        self.P4_1 = nn.Conv2d(C4_size, feature_size, kernel_size=1, stride=1, padding=0)
+        self.P4_2 = AdainResBlkGanilla(feature_size, feature_size, style_dim)
+
+        # add P4 elementwise to C3
+        self.P3_1 = nn.Conv2d(C3_size, feature_size, kernel_size=1, stride=1, padding=0)
+        self.P3_2 = AdainResBlkGanilla(feature_size, feature_size, style_dim)
+
+        self.P2_1 = nn.Conv2d(C2_size, feature_size, kernel_size=1, stride=1, padding=0)
+        self.P2_upsampled = nn.Upsample(scale_factor=2, mode='nearest')
+        self.rp4 = nn.ReflectionPad2d(1)
+        self.P2_2 = nn.Conv2d(int(feature_size), int(feature_size/2), kernel_size=3, stride=1, padding=0)
+
+
+    def forward(self, inputs, s):
+        C2, C3, C4, C5 = inputs
+
+        i = 0
+        P5_adain_x = self.P5_2(C5, s)
+
+        i += 1
+        P4_x = self.P4_1(C4)
+        P4_x = P5_adain_x + P4_x
+        P4_adain_x = self.P4_2(P4_x, s)
+
+        i += 1
+        P3_x = self.P3_1(C3)
+        P3_x = P3_x + P4_adain_x
+        P3_adain_x = self.P3_2(P3_x, s)
+
+        i += 1
+        P2_x = self.P2_1(C2)
+        P2_x = P2_x + P3_adain_x
+        P2_upsampled_x = self.P2_upsampled(P2_x)
+        P2_x = self.rp4(P2_upsampled_x)
+        P2_x = self.P2_2(P2_x)
+
+        return P2_x
+
+class GeneratorGanilla(nn.Module):
+
+    def __init__(self, img_size=256,  style_dim=64, input_nc=3, output_nc=3, ngf=64, ganilla_layer_nb=[2, 2, 2, 2], use_dropout=True, masks=None):
+        super().__init__()
+
+        ganilla_block_nf = [64, 128, 128, img_size]
+        self.inplanes = ngf
+
+        # first conv
+        self.pad1 = nn.ReflectionPad2d(input_nc)
+        self.conv1 = nn.Conv2d(input_nc, ngf, kernel_size=7, stride=1, padding=0, bias=True)
+        self.in1 = nn.InstanceNorm2d(ngf)
+        self.relu = nn.ReLU(inplace=True)
+        self.pad2 = nn.ReflectionPad2d(1)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)
+
+        # Output layer
+        self.pad3 = nn.ReflectionPad2d(output_nc)
+        self.conv2 = nn.Conv2d(64, output_nc, 7)
+        self.tanh = nn.Tanh()
+
+        self.layer1 = self._make_layer_ganilla(BasicBlock_Ganilla, ganilla_block_nf[0], ganilla_layer_nb[0], use_dropout, stride=1)
+        self.layer2 = self._make_layer_ganilla(BasicBlock_Ganilla, ganilla_block_nf[1], ganilla_layer_nb[1], use_dropout, stride=2)
+        self.layer3 = self._make_layer_ganilla(BasicBlock_Ganilla, ganilla_block_nf[2], ganilla_layer_nb[2], use_dropout, stride=2)
+        self.layer4 = self._make_layer_ganilla(BasicBlock_Ganilla, ganilla_block_nf[3], ganilla_layer_nb[3], use_dropout, stride=2)
+
+        # for the skip connections
+        fpn_sizes = [self.layer1[ganilla_layer_nb[0] - 1].conv2.out_channels,
+                     self.layer2[ganilla_layer_nb[1] - 1].conv2.out_channels,
+                     self.layer3[ganilla_layer_nb[2] - 1].conv2.out_channels,
+                     self.layer4[ganilla_layer_nb[3] - 1].conv2.out_channels]
+
+        self.fpn = PyramidFeatures(fpn_sizes[0], fpn_sizes[1], fpn_sizes[2], fpn_sizes[3], style_dim=style_dim)
+
+    def _make_layer_ganilla(self, block, planes, blocks, use_dropout, stride=1):
+        strides = [stride] + [1] * (blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inplanes, planes, use_dropout, stride))
+            self.inplanes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, inputs, s, masks=None):
+
+        img_batch = inputs
+
+        x = self.pad1(img_batch)
+        x = self.conv1(x)
+        x = self.in1(x)
+        x = self.relu(x)
+        x = self.pad2(x)
+        x = self.maxpool(x)
+
+        x1 = self.layer1(x)
+        x2 = self.layer2(x1)
+        x3 = self.layer3(x2)
+        x4 = self.layer4(x3)
+
+        out = self.fpn([x1, x2, x3, x4], s)
+
+        out = self.pad3(out)
+        out = self.conv2(out)
+        out = self.tanh(out)
+
+        return out
 
 class MappingNetwork(nn.Module):
     def __init__(self, latent_dim=16, style_dim=64, num_domains=2):
@@ -280,7 +481,8 @@ class Discriminator(nn.Module):
 
 
 def build_model(args):
-    generator = Generator(args.img_size, args.style_dim, w_hpf=args.w_hpf)
+    #generator = Generator(args.img_size, args.style_dim, w_hpf=args.w_hpf)
+    generator = GeneratorGanilla(img_size=args.img_size, style_dim=args.style_dim)
     mapping_network = MappingNetwork(args.latent_dim, args.style_dim, args.num_domains)
     style_encoder = StyleEncoder(args.img_size, args.style_dim, args.num_domains)
     discriminator = Discriminator(args.img_size, args.num_domains)
